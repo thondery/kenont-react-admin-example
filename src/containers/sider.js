c70642eeb483de88c79e332771f6668a0ad11158
thondery <thondery@163.com>
2018-04-24 10:36:46 +0800

    update sider

--- src/containers/sider.js
+++ src/containers/sider.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Sider } from 'components'
-import { Icon } from 'antd'
+import { Icon, Tooltip } from 'antd'
 import * as menuSubs from 'features/menuSub'
 
 export default ({ location }) => (
   <Sider 
     {...{ location, menuSubs }}
     footer={ (collapsed) => (
-      <a href="https://github.com/thondery/kenont-react-admin-example" target="_blank">
-        <Icon type="github" style={!collapsed ? { fontSize: '16px' } : null} />
-        {!collapsed && <span>github/thondery</span>}
-      </a>
+      <Tooltip 
+        title={(
+          <div>
+            <Icon type="github" style={{ fontSize: '16px', marginRight: 10 }} />
+            <span>github/thondery</span>
+          </div>
+        )}
+        >
+        <a href="https://github.com/thondery/kenont-react-admin-example" target="_blank">
+          <Icon type="github" style={!collapsed ? { fontSize: '16px' } : null} />
+          {!collapsed && <span>github/thondery</span>}
+        </a>
+      </Tooltip>
     )}
   />
 )
